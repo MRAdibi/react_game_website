78ff074aa5f0f99b0bd8e92b9b34ca1d04a6f3ce
MRAdibi <m.r.adibi125@gmail.com>
2023-05-16 14:33:51 +0430

    change the style of the meracritic score component

--- src/components/MetacriticScore.tsx
+++ src/components/MetacriticScore.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 function MetacriticScore({ score }: Props) {
   const color = score > 75 ? "green" : score > 60 ? "yellow" : "";
-  return <Badge colorScheme={color}>{score}</Badge>;
+  return <Badge colorScheme={color} fontSize={"14px"} paddingX={2} borderRadius="4px">{score}</Badge>;
 }
 
 export default MetacriticScore;
