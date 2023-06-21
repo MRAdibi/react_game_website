648f2c33a08a98478b7e718f91ace410bac864bf
mohammad <m.r.adibi125@gmail.com>
2023-06-21 22:46:53 +0330

    change the dark theme

--- src/theme.ts
+++ src/theme.ts
@@ -4,6 +4,22 @@ const config: ThemeConfig = {
   initialColorMode: "dark",
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({
+  config,
+  colors: {
+    gray: {
+      50: "#edf2fc",
+      100: "#d4d8e0",
+      200: "#b9bec6",
+      300: "#9ea4ae",
+      400: "#838a96",
+      500: "#68707c",
+      600: "#515761",
+      700: "#393e46",
+      800: "#21252c",
+      900: "#070c15",
+    },
+  },
+});
 
 export default theme;
