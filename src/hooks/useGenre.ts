53984082eca531875b4a0e20bf92514d45167592
MRAdibi <m.r.adibi125@gmail.com>
2023-11-25 09:22:18 +0330

    make the api-client reusable

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
-import apiClient from "../services/api-client";
-import { FetchResponse } from "../services/api-client";
+import APIClient from "../services/api-client";
+
+const apiClient = new APIClient<Genre>("/genres");
 
 export interface Genre {
   id: number;
@@ -11,8 +12,7 @@ export interface Genre {
 const useGenre = () =>
   useQuery({
     queryKey: ["genrs"],
-    queryFn: () =>
-      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+    queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, // 24h
   });
 
