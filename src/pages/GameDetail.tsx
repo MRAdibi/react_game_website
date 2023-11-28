d63a7d5a1a497e6b77c4d47b2b766b6e9284bb85
MRAdibi <m.r.adibi125@gmail.com>
2023-11-28 23:16:23 +0330

    build expandable text and also change the theme

--- src/pages/GameDetail.tsx
+++ src/pages/GameDetail.tsx
@@ -1,6 +1,7 @@
-import useGame from "../hooks/useGame";
+import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import ExpandableText from "../components/ExpandableText";
+import useGame from "../hooks/useGame";
 
 function GameDetail() {
   const { slug } = useParams();
@@ -13,7 +14,7 @@ function GameDetail() {
 
   return <>
     <Heading>{game.name}</Heading>
-    <Text>{game.description_raw}</Text>
+    <ExpandableText>{game.description_raw}</ExpandableText>
   </>
 }
 
